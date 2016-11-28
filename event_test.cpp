ab9245bdf6745dd57b1dc879d59b9184d9a78681
Chad Baily <chadbaily27@gmail.com>
2016-11-28 02:15:06 -0500

    Event test cases

--- event_test.cpp
+++ event_test.cpp
@@ -15,8 +15,32 @@ TEST(event, getStartDate)
 {
     Event e = new Event(0);
 
-     ASSERT_EQ(true, e->getStartDate() == "2016-11-22 15:33:00");
+    ASSERT_EQ(true, e->getStartDate() == "2016-11-22 15:33:00");
 }
 
+TEST (event, getEndDate())
+{
+    Event e = new Event(0);
 
-    
+    ASSERT_EQ(true, e->getEndDate() == "2016-11-22 16:33:00");
+}
+
+TEST (event, getDescription)
+{
+    Event e = new Event(0);
+
+    ASSERT_EQ(true, e->getDescription() == "A great event for friends and family");
+}
+
+TEST (event, getLocation)
+{
+    Event e = new Event(0);
+
+    ASSERT_EQ(true, e->getLocation() == "Stetson U");
+}
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
