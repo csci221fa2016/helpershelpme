58ac953a951546731ff5256c3ffb6a373c09b118
Duncan McMahon <dmcmahon@stetson.edu>
2016-11-28 01:03:45 -0500

    all getters have something

--- event.cpp
+++ event.cpp
@@ -16,4 +16,14 @@ string Event::getStartDate() {
 User* Event::getOrganizer() {
     return new User(1);
 }
+string Event::getEndDate() {
+	return "2016-11-22 16:33:00";
+}
+string Event::getDescription() {
+	return "A great event for friends and family";
+}
+
+string Event::getLocation() {
+	return "Stetson U";
+}
 
