a297b0a1959ccf1f688c3d8545f33fcc7ba3895f
Chris Finkle <chrisdfinkle@gmail.com>
2016-11-30 16:56:53 -0500

    changes to event and eventposition

--- event.cpp
+++ event.cpp
@@ -3,7 +3,45 @@
 #include "user.h"
 #include "eventposition.h"
 
-Event::Event(int _eventid) : eventid(_eventid) {}
+Event::Event(int _eventid) : eventid(_eventid) {
+	char *errmsg;
+	retval = sqlite3_open("/tmp/csci221_dmcmahon.db", &db);
+	if(retval != 0)
+	{
+		cout << "Cannot open test.db: " << sqlite3_errcode(db) << endl;
+		return;
+	}
+	retval = sqlite3_exec(db, "create table if not exists event (eventid integer primary key, 
+name text, description text, start text, end text, organizer integer, location text);", NULL, NULL, &errmsg);
+	if(retval != SQLITE_OK)
+	{
+		cout << "Error in previous command: " << errmsg << endl;
+		sqlite3_free(errmsg);
+	}
+
+	//make sure that event exists in db
+	bool exists = false;
+	sqlite3_stmt *s;
+	const char *sql = "select * from events where eventid = ?"'
+	retval = sqlite3_prepare(db, sql, strlen(sql), &s, NULL);
+	if(retval != SQLITE_OK) {
+		cout << "Error in SQL statement " << sql;
+		return;
+	}
+    retval = sqlite3_bind_int(s, 1, eventid);
+    if(retval != SQLITE_OK) {
+        cout << "Error in binding SQL statement " << sql;
+        return;
+    }
+	if(sqlite3_step(s) == SQLITE_ROW) {
+        exists = true;
+	}
+    sqlite3_reset(s);
+	if(!exists) {
+		sqlite3_stmt *s;
+		//TODO const char *sql = "insert into 
+	}
+}
 
 string Event::getName() {
     return "Joe Blow";
