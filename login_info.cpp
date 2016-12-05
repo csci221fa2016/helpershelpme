a881449d4eacdd90c4c3008f9a3c017087d2b5f8
luisa M <lmolina@stetson.edu>
2016-12-04 23:09:48 -0500

    commented out the sections of login_info that won't connect cleanly with controller due to other issues

--- login_info.cpp
+++ login_info.cpp
@@ -19,8 +19,9 @@ using namespace cgicc;
 int main () {
     Cgicc cgi;
     HTTPCookie c1;
+    //<=================================================CGI DATA PROCESS======================>    
     //retrieves form information and sends it to controller
-    vector<string> loginfo;
+/*    vector<string> loginfo;
     Controller control;
     const_form_iterator phoneNum = cgi.getElement("pnumber");
     if(phoneNum != (*cgi).end() && !phoneNum->isEmpty())
@@ -39,17 +40,15 @@ int main () {
     } else {
         cout << "<p> Username or Password Invalid </p>" << endl;
         cout << HTTPRedirectHeader("/view/lulu/login.cgi") <<endl;
-    }
-//    cout << HTTPHTMLHeader() << HTMLDoctype(HTMLDoctype::eStrict) << endl;
+    }*/
+    cout << HTTPHTMLHeader() << HTMLDoctype(HTMLDoctype::eStrict) << endl;
     cout << "<html>\n";
     cout << "<head>\n";
     cout << "<title>Redirecting</title>\n";
     cout << "</head>\n";
     cout << "<body>\n";
-    //<==================================================================CGI DATA PROCESS======================================>    
-    vector<string> login;
     //===========================================================Log in==============================================>
-/*    form_iterator it = cgi.getElement("user_phone_number");  
+    form_iterator it = cgi.getElement("user_phone_number");  
     if( !it->isEmpty() && it != (*cgi).end()) {  
         cout <<"<h2>Phone Number:: " << **it<<" </h2>" << endl;
         login.push_back(**it);      
@@ -64,7 +63,8 @@ int main () {
         cout << "<h2>No text entered for last name,</h2>" << endl;  
     }
     cout<<"<p> Your phone number is /"<<login[0]<<"/ and yourt password is /"<<login[1]<<"/</p>"<<endl;
-    c=:*/
+//    c=:
+
     cout << "</body>\n";
     cout << "</html>\n";
     
