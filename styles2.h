2118c999633ae9d7d4a46ce40b394da1d5009b54
JUAN <jtoledoc@stetson.edu>
2016-12-08 15:54:07 -0500

    Update v3.8.1
    Fixed luisa's styles, log out only works in master(don't tuch)

---
+++ styles2.h
@@ -0,0 +1,25 @@
+#ifndef _STYLES_H_
+#define _STYLES_H_ 1
+#include <string>
+
+const std::string styles =
+"#header.alt {\
+    -moz-animation: none;\
+    -webkit-animation: none;\
+    -ms-animation: none;\
+    animation: none;\
+    background: #fffffff;\
+    box-shadow: none;\
+    color: #000;\
+    padding: 2em 2.5em;\
+    position: absolute;\
+body, input, select, textarea {\
+    color: #000000;\
+    font-family: 'Lato', sans-serif;\
+    font-size: 15pt;\
+    font-weight: 300;\
+    letter-spacing: 0.025em;\
+    line-height: 1.75em;\
+}"
+;
+#endif
