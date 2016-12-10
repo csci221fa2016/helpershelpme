8c97e6c15e8340f54ebe7e9933919d9dfa5ce696
Chris Finkle <chrisdfinkle@gmail.com>
2016-12-10 12:17:15 -0500

    fixed declaration in creation.h

--- creation.h
+++ creation.h
@@ -16,7 +16,7 @@ class Creation {
 	bool findUser(int _userid);
 	bool findEvent(int _eventid);
 	int createUser(string _name, string _phoneNumber, string _password);
-	int createEvent(string _name, string _description, time_t _startTime, time_t _endTime, int userid, string _location);
+	int createEvent(string _name, string _description, time_t _startTime, time_t _endTime, string _location);
 	int createEventPosition(int eventid, int posid, int userid);
 	vector<int> getUpcoming();
 	void createVacancy(int eventid, int posid, string name, int openings);
