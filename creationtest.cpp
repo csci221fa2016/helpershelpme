634f83ece4c5d110cf82402fe48930c1fc0ced78
Duncan McMahon <dmcmahon@stetson.edu>
2016-12-14 03:00:42 -0500

    creationtest

---
+++ creationtest.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "creation.h"
+#include "gtest/gtest.h"
+
+TEST(creation, login) {
+	Creation* c = new Creation();
+	c->createUser("dman", "4074933565", "password");
+	int l = c->logIn("4074933565","password");
+	//cout<<"the userid is: "<<l<<endl;
+	ASSERT_EQ(true, l != 0);
+}
+int main(int argc, char **argv) {
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
